4190218ad024876aa3334810ac3c2211464781ee
swagashek1 <am93029@uga.edu>
2022-02-10 16:34:39 -0500

    git commit-m"made methods and added user input"

--- DrawPoints.java
+++ DrawPoints.java
@@ -1,82 +1,96 @@
 import java.awt.*;
 import javax.swing.*;
+import java.util.*;
 
 public class DrawPoints extends JPanel {
+	static ArrayList<ArrayList<Integer>> listr = new ArrayList<ArrayList<Integer>>();
 	public void paintComponent(Graphics g) {
+		for(int i=0; i< listr.get(0).size(); i++){
+			int x = listr.get(0).get(i);
+			int y= listr.get(1).get(i);
+			g.drawLine(x, y, x, y);
+		}
+		// g.drawLine(x1, y1, x2, y2);
+	}
 
-		int range = 500;
-		for (int i = 0; i < 5000; i++) {
-
+	public static void basicAlg() {
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("how many lines");
+			int count = sc.nextInt();
+			int range = 500;
+			ArrayList<Integer> arrlx = new ArrayList<Integer>();
+			ArrayList<Integer> arrly = new ArrayList<Integer>();
 			
-			  int x1 = (int) (Math.random() * range + 100),
-			  y1 = (int) (Math.random() * range + 100),
-			  x2 = (int) (Math.random() * range + 100),
-			  y2 = (int) (Math.random() * range + 100);
-			 
-			int m = 1;
-			if (y2 == y1) {
-				if (x2 < x1) {
-					int temp = x1;
-					x1 = x2;
-					x2 = temp;
-				}
-				for (int x = x1; x < x2; x++) {
-					g.drawLine(x, y1, x, y1);
-				}
-			} else if (x1 == x2) {
-				if (y2 < y1) {
-					int temp = y1;
-					y1 = y2;
-					y2 = temp;
-				}
-				for (int y = y1; y < y2; y++) {
-					g.drawLine(x1, y, x1, y);
-				}
-			} else {
-				m = Math.round((y2 - y1) / (x2 - x1));
-				if (m <= 1 && m >= -1) {
-					double m1 = (double)(y2 - y1) / (x2 - x1);
+
+			for (int i = 0; i < count; i++) {
+
+				int x1 = (int) (Math.random() * range + 100),
+						y1 = (int) (Math.random() * range + 100),
+						x2 = (int) (Math.random() * range + 100),
+						y2 = (int) (Math.random() * range + 100);
+
+				int m = 1;
+				if (y2 == y1) {
 					if (x2 < x1) {
-						int temp = y1, temp1 = x1;
-						y1 = y2;
+						int temp = x1;
 						x1 = x2;
-						y2 = temp;
-						x2 = temp1;
+						x2 = temp;
 					}
-					double c = (double)y1 - (m1 * (double)x1);
 					for (int x = x1; x < x2; x++) {
-						int y = (int)Math.round((m1 * x )+ c);
-						g.drawLine(x, y, x, y);
+						arrlx.add(x);
+						arrly.add(y1);
 					}
-				} else if (m > 1) {
+				} else if (x1 == x2) {
 					if (y2 < y1) {
-						int temp = y1, temp1 = x1;
+						int temp = y1;
 						y1 = y2;
-						x1 = x2;
 						y2 = temp;
-						x2 = temp1;
 					}
-					int c = y1 - m * x1;
 					for (int y = y1; y < y2; y++) {
-						int x = Math.round((y - c) / m);
-						g.drawLine(x, y, x, y);
+						arrlx.add(x1);
+						arrly.add(y);
 					}
-				} else if (m < -1) {
-					if (y2 < y1) {
-						int temp = y1, temp1 = x1;
-						y1 = y2;
-						x1 = x2;
-						y2 = temp;
-						x2 = temp1;
+				} else {
+					m = Math.round((y2 - y1) / (x2 - x1));
+					if (m <= 1 && m >= -1) {
+						double m1 = (double) (y2 - y1) / (x2 - x1);
+						if (x2 < x1) {
+							int temp = y1, temp1 = x1;
+							y1 = y2;
+							x1 = x2;
+							y2 = temp;
+							x2 = temp1;
+						}
+						double c = (double) y1 - (m1 * (double) x1);
+						for (int x = x1; x < x2; x++) {
+							int y = (int) Math.round((m1 * x) + c);
+							arrlx.add(x);
+							arrly.add(y);
+						}
+					} else if (m > 1 || m < 1) {
+						if (y2 < y1) {
+							int temp = y1, temp1 = x1;
+							y1 = y2;
+							x1 = x2;
+							y2 = temp;
+							x2 = temp1;
+						}
+						int c = y1 - m * x1;
+						for (int y = y1; y < y2; y++) {
+							int x = Math.round((y - c) / m);
+							arrlx.add(x);
+							arrly.add(y);
+						}
 					}
-					int c = y1 - m * x1;
-					for (int y = y1; y < y2; y++) {
-						int x = Math.round((y - c) / m);
-						g.drawLine(x, y, x, y);
-					}
+
 				}
 			}
+			listr.add(arrlx);
+			listr.add(arrly);	
+			
 		}
+
+
 	}
 
 	public static void main(String[] args) {
@@ -85,6 +99,7 @@ public class DrawPoints extends JPanel {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setBackground(Color.white);
 		frame.setSize(800, 800);
+		basicAlg();
 
 		DrawPoints panel = new DrawPoints();
 
--- DrawpointsBresenham.java
+++ DrawpointsBresenham.java
@@ -1,49 +1,84 @@
 import java.awt.*;
 import javax.swing.*;
+import java.util.*;
  
-class DrawpointsBresenham extends JPanel {
+public class DrawpointsBresenham extends JPanel {
+    static ArrayList<ArrayList<Integer>> listr = new ArrayList<ArrayList<Integer>>();
     public void paintComponent(Graphics g) {
-        int range = 500;
-        for (int i = 0; i < 5000; i++) {
-            int x1 = (int) (Math.random() * range + 100),
-                    y1 = (int) (Math.random() * range + 100),
-                    x2 = (int) (Math.random() * range + 100),
-                    y2 = (int) (Math.random() * range + 100);
-            // CODE FROM GEEKSFORGEEKS
-            // https://www.geeksforgeeks.org/bresenhams-line-generation-algorithm/
-            int m_new = 2 * (y2 - y1);
-            int slope_error_new = m_new - (x2 - x1);
- 
-            for (int x = x1, y = y1; x <= x2; x++) {
-                g.drawLine(x, y, x, y);
- 
-                // Add slope to increment angle formed
-                slope_error_new += m_new;
- 
-                // Slope error reached limit, time to
-                // increment y and update slope error.
-                if (slope_error_new >= 0) {
-                    y++;
-                    slope_error_new -= 2 * (x2 - x1);
-                }
-            }
+        for(int i=0; i< listr.get(0).size(); i++){
+            int x = listr.get(0).get(i);
+            int y= listr.get(1).get(i);
+            g.drawLine(x, y, x, y);
            
+        }
+        // g.drawLine(x1, y1, x2, y2);
+    }
+ 
+    public static void brez() {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("how many lines");
+            int count = sc.nextInt();
+            int range = 500;
+            ArrayList<Integer> arrlx = new ArrayList<Integer>();
+            ArrayList<Integer> arrly = new ArrayList<Integer>();
+            for (int i = 0; i < count; i++) {
+                int x1 = (int) (Math.random() * range + 100),
+                        y1 = (int) (Math.random() * range + 100),
+                        x2 = (int) (Math.random() * range + 100),
+                        y2 = (int) (Math.random() * range + 100);
+                // CODE FROM GEEKSFORGEEKS
+                // https://www.geeksforgeeks.org/bresenhams-line-generation-algorithm/
+                if(x2<x1){
+                    int temp = y1, temp1 = x1;
+							y1 = y2;
+							x1 = x2;
+							y2 = temp;
+							x2 = temp1;
+                }
+                int m_new = 2 * (y2 - y1);
+                int slope_error_new = m_new - (x2 - x1);
+ 
+                for (int x = x1, y = y1; x <= x2; x++) {
+                    arrlx.add(x);
+                    arrly.add(y);
+ 
+                    // Add slope to increment angle formed
+                    slope_error_new += m_new;
+ 
+                    // Slope error reached limit, time to
+                    // increment y and update slope error.
+                    if (slope_error_new >= 0) {
+                        y++;
+                        slope_error_new -= 2 * (x2 - x1);
+                    }
+                }
+ 
+            }
+            listr.add(arrlx);
+            listr.add(arrly);  
+       
  
         }
     }
  
+ 
     public static void main(String[] args) {
+       
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame("Draw Points");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBackground(Color.white);
         frame.setSize(800, 800);
+        brez();
  
         DrawpointsBresenham panel = new DrawpointsBresenham();
+        System.out.println(listr.get(0).size()+"  next    ");
  
         frame.add(panel);
  
         frame.setVisible(true);
     }
+ 
 }
-
+ 
+ 
